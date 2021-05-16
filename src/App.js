55bfcf4b13efbebb8ec3ac0cc9499aaea986d548
starhn87 <starhn87@gmail.com>
2021-05-16 23:22:55 +0900

    to do with useReducer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { useReducer } from "react";
-import Screen from "./Screen";
-import Lang from "./context";
-import translations from "./translations";
-
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
-  }
-}
+import React, { useReducer, useState } from "react";
+import reducer, {
+  initialState,
+  ADD,
+  DEL,
+  COMPLETE,
+  UNCOMPLETE
+} from "./reducer";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map(toDo => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              ❌
+            </button>
+            <button
+              onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}
+            >
+              ✅
+            </button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map(toDo => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <button
+                  onClick={() => dispatch({ type: DEL, payload: toDo.id })}
+                >
+                  ❌
+                </button>
+                <button
+                  onClick={() =>
+                    dispatch({ type: UNCOMPLETE, payload: toDo.id })
+                  }
+                >
+                  🙅🏼‍♂️
+                </button>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
     </>
   );
 }
