30f22852277b04b40e4dae7c331039794f9bca37
starhn87 <starhn87@gmail.com>
2021-05-18 00:29:28 +0900

    refactoring todo

--- src/context.js
+++ src/context.js
@@ -1,33 +1,25 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useReducer, useContext } from "react";
+import reducer, { initialState } from "./reducer";
 
-const LangContext = createContext();
+const ToDosContext = createContext();
 
-const Lang = ({ defaultLang, children, translations }) => {
-  const [lang, setLang] = useState(defaultLang);
-  const hyperTranslate = (text) => {
-    if (lang === defaultLang) {
-      return text;
-    } else {
-      return translations[lang][text];
-    }
-  }
-
+const ToDosProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <LangContext.Provider value={{ setLang, t: hyperTranslate }}>
+    <ToDosContext.Provider value={{ state, dispatch }}>
       {children}
-    </LangContext.Provider>
-  )
-}
+    </ToDosContext.Provider>
+  );
+};
 
-export const useSetLang = () => {
-  const { setLang } = useContext(LangContext);
+export const useDispatch = () => {
+  const { dispatch } = useContext(ToDosContext);
+  return dispatch;
+};
 
-  return setLang;
-}
+export const useState = () => {
+  const { state } = useContext(ToDosContext);
+  return state;
+};
 
-export const useT = () => {
-  const { t } = useContext(LangContext);
-  return t;
-}
-
-export default Lang;
+export default ToDosProvider;
